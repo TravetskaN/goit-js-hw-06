7a7ba3fff553ff8417fc01e02a9d6c5f83e71923
TravetskaN <ntravetskaya@gmail.com>
2022-01-12 15:42:42 +0200

    дз

--- js/task-05.js
+++ js/task-05.js
@@ -3,8 +3,9 @@
 const inputRef = document.querySelector("#name-input");
 const outputRef = document.querySelector("#name-output");
 
-function handleInputChange(event) {
-  outputRef.textContent = event.target.value;
-}
+inputRef.addEventListener("input", (event) => {
+  event.preventDefault();
+  const inputName = event.target.value;
 
-inputRef.addEventListener("input", handleInputChange);
+  outputRef.textContent = inputName.length === 0 ? "Anonymous" : inputName;
+});
